26fdee36588bd8b18b68a9c375e07a9e7871fcb3
malrangjeongha <gorus132@gmail.com>
2023-03-27 16:17:12 +0900

    feat: 루트 스택 정리

--- lib/views/pages/login/login_checkmail_page.dart
+++ lib/views/pages/login/login_checkmail_page.dart
@@ -5,61 +5,78 @@ import 'package:flutter/material.dart';
 import '../../../models/members.dart';
 import '../../components/button.dart';
 
-class LoginCheckMail extends StatefulWidget{
+class LoginCheckMail extends StatefulWidget {
   const LoginCheckMail({super.key});
 
   @override
   State<LoginCheckMail> createState() => _LoginCheckMailState();
 }
 
-class _LoginCheckMailState extends State<LoginCheckMail>{
-
-  Widget build(BuildContext context){
-
+class _LoginCheckMailState extends State<LoginCheckMail> {
+  Widget build(BuildContext context) {
     return Scaffold(
-      appBar: CustomAppBar(appBar: AppBar(), title: '', onPressed: () {},),
-      body: Padding(
-        padding: const EdgeInsets.only(right: 20,left: 20, bottom: 50, top: 120),
-        child: Center(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Icon(Icons.check_circle, size: 80,),
-              SizedBox(height: 50),
-              Text('메일을 확인해주세요.',textAlign: TextAlign.center,
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
-              Text('작성하신 메일로 임시비밀번호를\n발급해드렸어요!\n\n다시 로그인 해주세요.', textAlign: TextAlign.center,
-              style: TextStyle(fontSize: 13),),
-              Expanded(child: SizedBox()),
-              Button(child: Text('로그인하기'),
-              onPressed: (){
-                Navigator.pushNamed(context, '/login');
-              },),
-              SizedBox(height: 10),
-              Container(
-                width : double.maxFinite,
-                height: 50,
-                child: ElevatedButton(
-                  onPressed:(){
-                    Navigator.pushNamed(context, '/findpassword');
-                  },
-                  child: Text('이메일 재입력하기', style: TextStyle(
-                    color: Colors.black
-                  ),),
-                  style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.white70,
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(40)
-                  )
-              ),
-            ),
-          )
-
-            ],
-          ),
+        appBar: CustomAppBar(
+          appBar: AppBar(),
+          title: '',
+          onPressed: () {},
         ),
-      )
-    );
+        body: Padding(
+          padding:
+              const EdgeInsets.only(right: 20, left: 20, bottom: 50, top: 120),
+          child: Center(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(
+                  Icons.check_circle,
+                  size: 80,
+                ),
+                SizedBox(height: 50),
+                Text(
+                  '메일을 확인해주세요.',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  '작성하신 메일로 임시비밀번호를\n발급해드렸어요!\n\n다시 로그인 해주세요.',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 13),
+                ),
+                Expanded(child: SizedBox()),
+                Button(
+                  child: Text('로그인하기'),
+                  onPressed: () {
+                    //스택 비우고
+                    Navigator.of(context)
+                        .pushNamedAndRemoveUntil('/start', (Route<dynamic> route) => false
+                    );
+                    //login페이지를 push
+                    Navigator.pushNamed(context, '/login');
+                  },
+                ),
+                SizedBox(height: 10),
+                Container(
+                  width: double.maxFinite,
+                  height: 50,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      //Navigator.pushNamed(context, '/findpassword');
+                      Navigator.pop(context);
+                    },
+                    child: Text(
+                      '이메일 재입력하기',
+                      style: TextStyle(color: Colors.black),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.white70,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(40))),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ));
   }
 }
--- lib/views/pages/login/login_page.dart
+++ lib/views/pages/login/login_page.dart
@@ -5,47 +5,54 @@ import 'package:flutter/material.dart';
 import '../../../models/members.dart';
 import '../../components/button.dart';
 
-class Login extends StatefulWidget{
+class Login extends StatefulWidget {
   const Login({super.key});
 
   @override
   State<Login> createState() => _LoginState();
 }
 
-class _LoginState extends State<Login>{
+class _LoginState extends State<Login> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   final TextEditingController _EmailController = TextEditingController();
   final TextEditingController _PasswordController = TextEditingController();
 
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     //Members members = new Members('','','','','','','','');
 
     return Scaffold(
       resizeToAvoidBottomInset: false,
-      appBar: CustomAppBar(appBar: AppBar(), title: '', onPressed: () {},),
+      appBar: CustomAppBar(
+        appBar: AppBar(),
+        title: '',
+        onPressed: () {},
+      ),
       body: Padding(
-        padding: EdgeInsets.only(right: 20,left: 20,top: 50,bottom: 70),
+        padding: EdgeInsets.only(right: 20, left: 20, top: 50, bottom: 70),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Expanded(
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    SizedBox(height: 20),
-                    Container(
-                      width: 100, height: 50,
-                      decoration: BoxDecoration(color: Colors.grey),
-                    ),
-                    Text('FRIEND SHIP', style: TextStyle(
-                      fontSize: 25, fontWeight: FontWeight.bold
-                    ),),
-                    Text('프렌즈쉽에 오신것을 환영합니다.', style: TextStyle(
-                      fontSize: 15
-                    ),),
-                    SizedBox(height: 20),
-                  ],
-                ),
+                children: [
+                  SizedBox(height: 20),
+                  Container(
+                    width: 100,
+                    height: 50,
+                    decoration: BoxDecoration(color: Colors.grey),
+                  ),
+                  Text(
+                    'FRIEND SHIP',
+                    style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    '프렌즈쉽에 오신것을 환영합니다.',
+                    style: TextStyle(fontSize: 15),
+                  ),
+                  SizedBox(height: 20),
+                ],
+              ),
             ),
             Expanded(
               child: Column(
@@ -99,28 +106,38 @@ class _LoginState extends State<Login>{
                 ),
             ),
             Button(
-                    child: Text('로그인'),
-                    onPressed: (){
-                      if(_formKey.currentState!.validate()){
+                child: Text('로그인'),
+                onPressed: () {
+                  /*
+                  해당 코드 오류가 생깁니다!!..
+                  if (_formKey.currentState!.validate()) {
+                  }
+                  */
 
-                      }
-                    }),
+                  //스택 비우고
+                  Navigator.of(context)
+                      .pushNamedAndRemoveUntil('/', (Route<dynamic> route) => false
+                  );
+                }),
             Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    Text('비밀번호를 잊으셨나요?'),
-                    TextButton(onPressed: (){
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Text('비밀번호를 잊으셨나요?'),
+                TextButton(
+                    onPressed: () {
                       Navigator.pushNamed(context, '/findpassword');
                     },
-                        child: Text('비밀번호 찾기',
-                        style: TextStyle(
-                            decoration: TextDecoration.underline,
-                            color: Colors.black, fontWeight: FontWeight.bold) ,))
-                  ],
-                )
-
+                    child: Text(
+                      '비밀번호 찾기',
+                      style: TextStyle(
+                          decoration: TextDecoration.underline,
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold),
+                    ))
               ],
-            ),
+            )
+          ],
+        ),
       ),
     );
   }
