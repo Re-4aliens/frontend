869171674824582e0a11f92923900a01465338d4
malrangjeongha <gorus132@gmail.com>
2023-05-04 11:53:13 +0900

    feat: 매칭 관련 mock data 추가

--- lib/mockdatas/mockdata_model.dart
+++ lib/mockdatas/mockdata_model.dart
@@ -1,1 +1,70 @@
-import '../models/member_model.dart';
+var token = const [
+  {
+    "accessToken": "213ss.s.21231232.axdasdas21451.1252134",
+    "refreshToken": "213ss.s.21231232.axdasdas21451.1252134"
+  }
+];
+
+var memberDetails = const [
+  {
+    "email": "exaple_user@exaple.com",
+    "mbti": "ENFJ",
+    "gender": "MALE",
+    "nationality": "Korea",
+    "birthday": "2000-01-01",
+    "name": "RYAN",
+    "profileImage": "url",
+    "age": 26
+  }
+];
+
+var status = const [
+  /*
+  경우에 따라
+  MATCHED, PENDING, NOT_APPLIED 입력
+   */
+  {"status": "MATCHED"}
+];
+
+var applicant = const [
+  {
+    "member": {
+      "name": "Jenny",
+      "gender": "Female",
+      "mbti": "INTJ",
+      "nationality": "Korea",
+      "age": 26,
+      "profileImage": "url/example",
+      "countryImage": "url/example"
+    },
+    "preferLanguages": {
+      "firstPreferLanguage": "string",
+      "secondPreferLanguage": "string"
+    }
+  }
+];
+
+var partners = const [
+  {
+    "members": [
+      {
+        "memberId": 1,
+        "name": "Jenny",
+        "mbti": "ENTJ",
+        "gender": "FEMALE",
+        "nationality": "Korean",
+        "profileImage": "url",
+        "countryImage": "url"
+      },
+      {
+        "memberId": 1,
+        "name": "Jenny",
+        "mbti": "ENTJ",
+        "gender": "FEMALE",
+        "nationality": "Korean",
+        "profileImage": "url",
+        "countryImage": "url"
+      },
+    ]
+  }
+];
