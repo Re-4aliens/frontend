d61029e9e14e92cacaf9eafee5a8970b823901fa
malrangjeongha <gorus132@gmail.com>
2023-05-04 10:59:18 +0900

    feat: 스플래시 스크린 & 로그인 수정

--- lib/views/pages/splash_page.dart
+++ lib/views/pages/splash_page.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/animation.dart';
 
@@ -13,23 +15,25 @@ class _SplashPageState extends State<SplashPage>
   late AnimationController controller;
   late Animation animation;
 
+  bool isStart = false;
+
   @override
   void initState() {
     super.initState();
-
-    controller =
-        AnimationController(vsync: this, duration: Duration(seconds: 2));
-
-    //여러가지 해봤는데 easeInOutBack이 제일 스무스해 보였어요
-    animation = CurvedAnimation(parent: controller, curve: Curves.easeInOutBack);
-
-    controller.repeat(reverse: true);
-
-    controller.addListener(() {
-      setState(() {
-      });
+    Timer(Duration(milliseconds: 10), () {
+      isStart = true;
     });
+
+
+    //4초 후에 넘어가기
+    Timer(Duration(milliseconds: 4000), () {
+      Navigator.pushNamed(context, '/');
+    });
+
+
+
   }
+
 
   @override
   void dispose() {
@@ -42,102 +46,51 @@ class _SplashPageState extends State<SplashPage>
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 80),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Container(
-              margin: EdgeInsets.symmetric(vertical: 5),
-              height: 30,
-              decoration: BoxDecoration(color: Colors.grey.shade400),
+      backgroundColor: Color(0xff7898FF),
+      body: Stack(
+        children: <Widget>[
+          AnimatedPositioned(
+            duration: Duration(milliseconds: 2000),
+            curve: Curves.elasticInOut,
+            child: Image.asset(
+              'assets/component_2.png',
+              width: 200,
+              height: 200,
             ),
-            Container(
-              margin: EdgeInsets.symmetric(vertical: 5),
-              height: 40,
-              decoration: BoxDecoration(color: Colors.grey.shade400),
+            left: isStart? 0 : -200,
+            top: isStart? 0 : -200,
+          ),
+
+          AnimatedPositioned(
+            duration: Duration(milliseconds: 2000),
+            curve: Curves.elasticInOut,
+            child: Image.asset(
+              'assets/component_1.png',
+              width: 120,
+              height: 120,
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Expanded(
-                  child: Container(
-                    alignment: Alignment.center,
-                    height: 200,
-                    decoration: BoxDecoration(color: Colors.grey.shade300),
-                    child: Stack(
-                      children: [
-                        Positioned(
-                          child: Container(
-                            width: 65,
-                            height: 80,
-                            decoration:
-                                BoxDecoration(color: Colors.grey.shade100),
-                          ),
-                          bottom: animation.value * 30 + 20,
-                          left: 0,
-                          right: 0,
-                        )
-                      ],
-                    ),
-                  ),
-                ),
-                Expanded(
-                  child: Container(
-                    height: 200,
-                    decoration: BoxDecoration(color: Colors.grey.shade300),
-                    child: Stack(
-                      children: [
-                        Positioned(
-                          child: Container(
-                            width: 65,
-                            height: 80,
-                            decoration:
-                                BoxDecoration(color: Colors.grey.shade100),
-                          ),
-                          bottom: animation.value * 50 + 20, //이 값을 다양하게 변형
-                          left: 0,
-                          right: 0,
-                        )
-                      ],
-                    ),
-                  ),
-                ),
-                Expanded(
-                  child: Container(
-                    height: 200,
-                    decoration: BoxDecoration(color: Colors.grey.shade300),
-                    child: Stack(
-                      children: [
-                        Positioned(
-                          child: Container(
-                            width: 65,
-                            height: 80,
-                            decoration:
-                                BoxDecoration(color: Colors.grey.shade100),
-                          ),
-                          bottom: animation.value * -50 + 60,
-                          left: 0,
-                          right: 0,
-                        )
-                      ],
-                    ),
-                  ),
-                ),
-              ],
+            right: isStart? 0 : -120,
+            top: 150,
+          ),
+
+          AnimatedPositioned(
+            duration: Duration(milliseconds: 2000),
+            curve: Curves.elasticInOut,
+            child: Image.asset(
+              'assets/character.png',
+              width: 400,
+              height: 300,
             ),
-            Container(
-              margin: EdgeInsets.symmetric(vertical: 5),
-              height: 10,
-              decoration: BoxDecoration(color: Colors.grey.shade400),
-            ),
-            Container(
-              margin: EdgeInsets.symmetric(vertical: 5),
-              height: 30,
-              decoration: BoxDecoration(color: Colors.grey.shade400),
-            ),
-          ],
-        ),
+            right: isStart? 0 : -400,
+            bottom: isStart? 0 : -300,
+          ),
+
+
+          //title
+          Center(
+              child: Image.asset('assets/titleImage.png',height: 80,)
+          )
+        ],
       ),
     );
   }
