6918573222c137c4a9cba614097c3702a20b18fb
malrangjeongha <gorus132@gmail.com>
2023-08-18 22:07:25 +0900

    design: 게시물 리스트 위젯

--- lib/views/pages/board/food_board_page.dart
+++ lib/views/pages/board/food_board_page.dart
@@ -10,6 +10,8 @@ import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
+import '../../../models/countries.dart';
+import '../../components/article_widget.dart';
 import '../../components/board_drawer_widget.dart';
 import 'article_page.dart';
 
@@ -81,135 +83,17 @@ class _FoodBoardPageState extends State<FoodBoardPage> {
           child: ListView.builder(
               itemCount: foodBoardList.length,
               itemBuilder: (context, index) {
+
+                var nationCode = '';
+                for (Map<String, String> country in countries) {
+                  if (country['name'] == foodBoardList[index].member!.nationality.toString()) {
+                    nationCode = country['code']!;
+                    break;
+                  }
+                }
                 return Column(
                   children: [
-                    ListTile(
-                      //제목
-                      title: Row(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.all(15.0),
-                            child: SvgPicture.asset(
-                              'assets/icon/icon_profile.svg',
-                              width: 35,
-                              color: Color(0xff7898ff),
-                            ),
-                          ),
-                          Text(
-                            '${foodBoardList[index].member!.nickname}',
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold, fontSize: 18),
-                          ),
-                          Text(
-                            '/',
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold, fontSize: 18),
-                          ),
-                          Text(
-                            'KR',
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold, fontSize: 18),
-                          )
-                        ],
-                      ),
-
-                      //내용
-                      subtitle: Container(
-                        padding: EdgeInsets.only(left: 15, bottom: 15),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            foodBoardList[index].imageUrls == null ?
-                            SizedBox():
-                            Container(
-                              height: 100,
-                              child: ListView.builder(
-                                  scrollDirection: Axis.horizontal,
-                                  itemCount: foodBoardList[index].imageUrls!.length,
-                                  itemBuilder: (context, index){
-                                    return Row(
-                                      children: [
-                                        Container(
-                                          margin: EdgeInsets.only(right: 10),
-                                          height: 80,
-                                          width: 80,
-                                          decoration: BoxDecoration(
-                                              color: Color(0xfff8f8f8),
-                                              borderRadius: BorderRadius.circular(10)
-                                          ),
-                                          child: Icon(Icons.add_photo_alternate_outlined),
-                                        ),
-                                      ],
-                                    );
-                                  }),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.only(top: 10, bottom: 25.0),
-                              child: Text('${foodBoardList[index].title}', style: TextStyle(fontSize: 16),),
-                            ),
-                            Row(
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              children: [
-                                Padding(
-                                  padding: const EdgeInsets.all(4.0),
-                                  child: Icon(
-                                    Icons.thumb_up_alt_sharp,
-                                    color: Color(0xffc1c1c1),
-                                    size: 20,
-                                  ),
-                                ),
-                                Padding(
-                                  padding: EdgeInsets.only(left: 4, right: 15),
-                                  child: foodBoardList[index].likeCount == 0
-                                      ? Text('')
-                                      : Text(
-                                      '${foodBoardList[index].likeCount}'),
-                                ),
-                                Padding(
-                                  padding: const EdgeInsets.all(4.0),
-                                  child: Icon(
-                                    Icons.chat_bubble,
-                                    color: Color(0xffc1c1c1),
-                                    size: 20,
-                                  ),
-                                ),
-                                Padding(
-                                  padding: const EdgeInsets.all(4.0),
-                                  child: Text(
-                                      '${foodBoardList[index].commentCount}'),
-                                ),
-                              ],
-                            )
-                          ],
-                        ),
-                      ),
-
-                      //더보기
-                      trailing: Row(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          Text(
-                            '1분 전',
-                            style: TextStyle(
-                                fontSize: 16, color: Color(0xffc1c1c1)),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.only(left: 8.0),
-                            child:
-                            Icon(Icons.more_vert, color: Color(0xffc1c1c1)),
-                          )
-                        ],
-                      ),
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) => ArticlePage(board: foodBoardList[index], boardCategory: "자유게시판")),
-
-                        );
-                      },
-                    ),
+                    ArticleWidget(board: foodBoardList[index], nationCode: nationCode,),
                     Divider(
                       thickness: 2,
                       color: Color(0xffE5EBFF),
--- lib/views/pages/board/music_board_page.dart
+++ lib/views/pages/board/music_board_page.dart
@@ -10,6 +10,8 @@ import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
+import '../../../models/countries.dart';
+import '../../components/article_widget.dart';
 import '../../components/board_drawer_widget.dart';
 import 'article_page.dart';
 
@@ -81,135 +83,16 @@ class _MusicBoardPageState extends State<MusicBoardPage> {
           child: ListView.builder(
               itemCount: musicBoardList.length,
               itemBuilder: (context, index) {
+                var nationCode = '';
+                for (Map<String, String> country in countries) {
+                  if (country['name'] == musicBoardList[index].member!.nationality.toString()) {
+                    nationCode = country['code']!;
+                    break;
+                  }
+                }
                 return Column(
                   children: [
-                    ListTile(
-                      //제목
-                      title: Row(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.all(15.0),
-                            child: SvgPicture.asset(
-                              'assets/icon/icon_profile.svg',
-                              width: 35,
-                              color: Color(0xff7898ff),
-                            ),
-                          ),
-                          Text(
-                            '${musicBoardList[index].member!.nickname}',
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold, fontSize: 18),
-                          ),
-                          Text(
-                            '/',
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold, fontSize: 18),
-                          ),
-                          Text(
-                            'KR',
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold, fontSize: 18),
-                          )
-                        ],
-                      ),
-
-                      //내용
-                      subtitle: Container(
-                        padding: EdgeInsets.only(left: 15, bottom: 15),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            musicBoardList[index].imageUrls == null ?
-                            SizedBox():
-                            Container(
-                              height: 100,
-                              child: ListView.builder(
-                                  scrollDirection: Axis.horizontal,
-                                  itemCount: musicBoardList[index].imageUrls!.length,
-                                  itemBuilder: (context, index){
-                                    return Row(
-                                      children: [
-                                        Container(
-                                          margin: EdgeInsets.only(right: 10),
-                                          height: 80,
-                                          width: 80,
-                                          decoration: BoxDecoration(
-                                              color: Color(0xfff8f8f8),
-                                              borderRadius: BorderRadius.circular(10)
-                                          ),
-                                          child: Icon(Icons.add_photo_alternate_outlined),
-                                        ),
-                                      ],
-                                    );
-                                  }),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.only(top: 10, bottom: 25.0),
-                              child: Text('${musicBoardList[index].title}', style: TextStyle(fontSize: 16),),
-                            ),
-                            Row(
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              children: [
-                                Padding(
-                                  padding: const EdgeInsets.all(4.0),
-                                  child: Icon(
-                                    Icons.thumb_up_alt_sharp,
-                                    color: Color(0xffc1c1c1),
-                                    size: 20,
-                                  ),
-                                ),
-                                Padding(
-                                  padding: EdgeInsets.only(left: 4, right: 15),
-                                  child: musicBoardList[index].likeCount == 0
-                                      ? Text('')
-                                      : Text(
-                                      '${musicBoardList[index].likeCount}'),
-                                ),
-                                Padding(
-                                  padding: const EdgeInsets.all(4.0),
-                                  child: Icon(
-                                    Icons.chat_bubble,
-                                    color: Color(0xffc1c1c1),
-                                    size: 20,
-                                  ),
-                                ),
-                                Padding(
-                                  padding: const EdgeInsets.all(4.0),
-                                  child: Text(
-                                      '${musicBoardList[index].commentCount}'),
-                                ),
-                              ],
-                            )
-                          ],
-                        ),
-                      ),
-
-                      //더보기
-                      trailing: Row(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          Text(
-                            '1분 전',
-                            style: TextStyle(
-                                fontSize: 16, color: Color(0xffc1c1c1)),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.only(left: 8.0),
-                            child:
-                            Icon(Icons.more_vert, color: Color(0xffc1c1c1)),
-                          )
-                        ],
-                      ),
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) => ArticlePage(board: musicBoardList[index], boardCategory: "자유게시판")),
-
-                        );
-                      },
-                    ),
+                    ArticleWidget(board: musicBoardList[index], nationCode: nationCode,),
                     Divider(
                       thickness: 2,
                       color: Color(0xffE5EBFF),
