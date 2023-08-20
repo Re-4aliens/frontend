2d25b2f3c18323de6dbf03bc3d9cbe821758e927
malrangjeongha <gorus132@gmail.com>
2023-08-20 16:18:15 +0900

    feat: 댓글 모델 수정 & 댓글 추가 기능

--- lib/mockdatas/comment_mockdata.dart
+++ lib/mockdatas/comment_mockdata.dart
@@ -1,5 +1,5 @@
 
-import '../models/Comment_model.dart';
+import '../models/comment_model.dart';
 
 
 List<Comment> commentListMock = [
@@ -12,7 +12,7 @@ List<Comment> commentListMock = [
 Comment comment1 = Comment(
     boardArticleCommentId: 1,
     content: "그러니까요 ㅠㅠㅠ 진짜 너무 더워요...",
-    member: Member(
+    member: CommentMember(
         memberId: 1,
         name: "누군가",
         nationality: "South Korea",
@@ -23,60 +23,69 @@ Comment comment1 = Comment(
     Comment(
       boardArticleCommentId: 1,
       content: "hot summer hot hot summer hot summer hot hot summer hot summer hot hot summer hot summer hot hot summer",
-      member: Member(
+      member: CommentMember(
         memberId: 1,
         name: "f(x)",
         nationality: "South Korea",
         email: "ex@email.com",
         profileImageUrl: ""
-      )
+      ),
+      createdAt: "2023-08-06 14:30:33",
     ),
     Comment(
         boardArticleCommentId: 1,
         content: "겨울 언제와...",
-        member: Member(
+        member: CommentMember(
             memberId: 1,
             name: "Daisy",
             nationality: "South Korea",
             email: "ex@email.com",
             profileImageUrl: ""
-        )
+        ),
+      createdAt: "2023-08-06 14:30:33",
     )
-  ]
+  ],
+  createdAt: "2023-08-06 14:30:33",
 );
 
 Comment comment2 = Comment(
     boardArticleCommentId: 1,
     content: "そうなんですよTT 本当に暑いです···",
-    member: Member(
+    member: CommentMember(
         memberId: 1,
         name: "ゆうき",
         nationality: "Japan",
         email: "ex@email.com",
         profileImageUrl: ""
-    )
+    ),
+  childs: [],
+  createdAt: "2023-08-06 14:30:33",
 );
 
 Comment comment3 = Comment(
     boardArticleCommentId: 1,
     content: "That's what I'm saying It's really hot...",
-    member: Member(
+    member: CommentMember(
         memberId: 1,
         name: "indigo",
         nationality: "Australia",
         email: "ex@email.com",
         profileImageUrl: ""
-    )
+    ),
+  childs: [],
+  createdAt: "2023-08-06 14:30:33",
 );
 
 Comment comment4 = Comment(
     boardArticleCommentId: 1,
     content: "就是啊 呜呜呜 真的太热了...",
-    member: Member(
+    member: CommentMember(
         memberId: 1,
         name: "釜庆大学生",
         nationality: "China",
         email: "ex@email.com",
         profileImageUrl: ""
-    )
+    ),
+  childs: [],
+  createdAt: "2023-08-06 14:30:33",
 );
