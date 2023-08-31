ed29f9ba055d68c369489216b87220f029444e63
KIM-JUHYEON-79 <jennykim6065@naver.com>
2023-08-31 22:48:18 +0900

    알림아이콘연결

--- lib/views/pages/board/food_board_page.dart
+++ lib/views/pages/board/food_board_page.dart
@@ -16,6 +16,7 @@ import '../../components/article_widget.dart';
 import '../../components/board_drawer_widget.dart';
 import 'article_page.dart';
 import 'article_writing_page.dart';
+import 'notification_page.dart';
 
 
 class FoodBoardPage extends StatefulWidget {
@@ -82,18 +83,30 @@ class _FoodBoardPageState extends State<FoodBoardPage> {
             ],
           ),
           actions: [
-            Padding(padding: EdgeInsets.all(8), child: SvgPicture.asset(
-              'assets/icon/ICON_notification.svg',
-              width: 28.r,
-              height: 28.r,
-              color: Colors.white,
-            ),),
+            Padding(
+              padding: EdgeInsets.all(8),
+              child: GestureDetector(
+                onTap: () {
+                  Navigator.push(context,
+                    MaterialPageRoute(
+                        builder: (context) => NotificationBoardWidget(screenArguments: widget.screenArguments)),
+                  );
+                },
+                child: SvgPicture.asset(
+                  'assets/icon/ICON_notification.svg',
+                  width: 28.r,
+                  height: 28.r,
+                  color: Colors.white,
+                ),
+              ),
+            ),
             Padding(padding: EdgeInsets.all(8), child: SvgPicture.asset(
               'assets/icon/icon_search.svg',
               width: 25.r,
               height: 25.r,
               color: Colors.white,
-            ),),
+            ),
+            ),
           ],
         ),
         body: isDrawerStart ? BoardDrawerWidget(screenArguments: widget.screenArguments, isTotalBoard: false,
--- lib/views/pages/board/notice_board_page.dart
+++ lib/views/pages/board/notice_board_page.dart
@@ -11,11 +11,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/svg.dart';
 
+import '../../../mockdatas/market_comment_mockdata.dart';
 import '../../../models/countries.dart';
 import '../../components/article_widget.dart';
 import '../../components/board_drawer_widget.dart';
 import '../../components/info_article_widget.dart';
 import '../../components/my_article_widget.dart';
+import '../../components/notification_widget.dart';
 import 'article_page.dart';
 import 'article_writing_page.dart';
 
@@ -35,74 +37,78 @@ class _NoticeBoardPageState extends State<NoticeBoardPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          title: Text(
-            'notice'.tr(),
-            style: TextStyle(
-              fontSize: 16.spMin,
-              color: Colors.white,
-            ),
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(
+          'notice'.tr(),
+          style: TextStyle(
+            fontSize: 16.spMin,
+            color: Colors.white,
           ),
-          toolbarHeight: 56.spMin,
-          elevation: 0,
-          shadowColor: Colors.black26,
-          backgroundColor: Color(0xff7898ff),
-          leadingWidth: 100,
-          leading: Column(
-            children: [
-              Row(
-                children: [
-                  IconButton(
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                    icon: SvgPicture.asset(
-                      'assets/icon/icon_back.svg',
-                      color: Colors.white,
-                      height: 18.h,
-                    ),
-                  ),
-                  IconButton(
-                    onPressed: () {
-                      setState(() {
-                        isDrawerStart = !isDrawerStart;
-                      });
-                    },
-                    icon: Icon(Icons.format_list_bulleted_outlined),
-                    color: Colors.white,
-                  ),
-                ],
-              )
-            ],
-          ),
-          actions: [
-            IconButton(onPressed: (){}, icon: Icon(Icons.notifications_none, size: 30.h,)),
-            IconButton(onPressed: (){}, icon: Icon(Icons.search, size: 30.h,)),
-          ],
         ),
-        body: isDrawerStart ?
-        BoardDrawerWidget(screenArguments: widget.screenArguments, isTotalBoard: false,
-          onpressd: (){},)
-            :Container(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Container(
-                padding: EdgeInsets.only(left: 21.w, right: 21.w, top: 27.h, bottom: 27.h),
-                child: Text('notice'.tr(),
-                  style: TextStyle(
-                    fontSize: 18.spMin,
-                    fontWeight: FontWeight.bold
+        toolbarHeight: 56.spMin,
+        elevation: 0,
+        shadowColor: Colors.black26,
+        backgroundColor: Color(0xff7898ff),
+        leadingWidth: 100,
+        leading: Column(
+          children: [
+            Row(
+              children: [
+                IconButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  icon: SvgPicture.asset(
+                    'assets/icon/icon_back.svg',
+                    color: Colors.white,
+                    height: 18.h,
                   ),
                 ),
-              ),
-              Divider(height: 1.h,color: Color(0xffcecece),),
+                IconButton(
+                  onPressed: () {
+                    setState(() {
+                      isDrawerStart = !isDrawerStart;
+                    });
+                  },
+                  icon: Icon(Icons.format_list_bulleted_outlined),
+                  color: Colors.white,
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+      body: isDrawerStart
+          ? BoardDrawerWidget(screenArguments: widget.screenArguments, isTotalBoard: false,
+        onpressd: (){},
+      )
+          :
 
-            ],
-          ),
+      Container(
+        decoration: BoxDecoration(color: Colors.white),
+        child: ListView.builder(
+            itemCount: NotificationList.length,
+            itemBuilder: (context, index) {
 
-        )
+              var nationCode = '';
+              for (Map<String, String> country in countries) {
+                if (country['name'] == NotificationList[index].member!.nationality.toString()) {
+                  nationCode = country['code']!;
+                  break;
+                }
+              }
+              return Column(
+                children: [
+                  NotificationWidget(board: NotificationList[index], nationCode: nationCode),
+                  Divider(
+                    thickness: 1.h,
+                    color: Color(0xffCECECE),
+                  )
+                ],
+              );
+            }),
+      ),
     );
   }
 }
