54ca3cbb9bb1032197801cbd31f861a372020643
sunga <tjddk6662@naver.com>
2024-07-04 19:41:51 +0900

    refactor : 머지 충돌 해결로 인한 오류 수정

--- lib/views/pages/board/free_posting_board_page.dart
+++ lib/views/pages/board/free_posting_board_page.dart
@@ -163,12 +163,8 @@ class _FreePostingBoardPageState extends State<FreePostingBoardPage> {
                       itemCount: boardProvider.articleList.length,
                       itemBuilder: (context, index) {
                         var nationCode = '';
-                        var member = boardProvider.articleList[index].member;
-
-                        var nationality = member?.nationality?.toString() ?? '';
 
                         for (Map<String, String> country in countries) {
-
                           if (country['name'] ==
                               boardProvider.articleList[index].memberProfileDto
                                   ?.nationality
--- lib/views/pages/board/info_board_page.dart
+++ lib/views/pages/board/info_board_page.dart
@@ -163,12 +163,8 @@ class _InfoBoardPageState extends State<InfoBoardPage> {
                       itemCount: boardProvider.articleList.length,
                       itemBuilder: (context, index) {
                         var nationCode = '';
-                        var member = boardProvider.articleList[index].member;
-
-                        var nationality = member?.nationality?.toString() ?? '';
 
                         for (Map<String, String> country in countries) {
-
                           if (country['name'] ==
                               boardProvider.articleList[index].memberProfileDto
                                   ?.nationality
