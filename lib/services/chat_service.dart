d074db88903e8d0586f5bb18f44d0718cde155ce
sunga <tjddk6662@naver.com>
2024-07-17 22:40:54 +0900

    refactor : 채팅 웹소켓 관련 api 수정
    테스트 x
    
    issues : #17

--- lib/services/chat_service.dart
+++ lib/services/chat_service.dart
@@ -1,75 +1,94 @@
 import 'dart:convert';
 import 'package:aliens/models/chat_room_model.dart';
 import 'package:http/http.dart' as http;
+import 'package:stomp_dart_client/stomp_config.dart';
 import 'api_service.dart';
 import 'package:aliens/models/message_model.dart';
-import 'package:aliens/services/auth_service.dart';
 import 'dart:async';
-import 'package:firebase_messaging/firebase_messaging.dart';
-import 'package:web_socket_channel/io.dart';
-import 'package:aliens/repository/sql_message_repository.dart';
-import 'package:aliens/models/partner_model.dart';
-import 'package:aliens/models/member_details_model.dart';
+import 'package:stomp_dart_client/stomp.dart';
+import 'package:stomp_dart_client/stomp_frame.dart';
 
 class ChatService extends APIService {
-  static late IOWebSocketChannel sendChannel;
-  static late IOWebSocketChannel readChannel;
-  static late IOWebSocketChannel bulkReadChannel;
+  static late StompClient stompClient;
+  static bool isConnected = false;
 
-  static List<Map> requestBuffer = [];
+  // static List<Map> requestBuffer = [];
+
+  static final StreamController<MessageModel> _messageController =
+      StreamController<MessageModel>.broadcast();
+  static Stream<MessageModel> get messageStream => _messageController.stream;
+
+  static final StreamController<int> _readReceiptController =
+      StreamController<int>.broadcast();
+  static Stream<int> get readReceiptStream => _readReceiptController.stream;
 
   /*
     
     웹소켓 연결 요청
 
   */
-  static Future<void> connectWebSocket(Partner partner,
-      MemberDetails memberDetails, Function updateUi, Function setState) async {
-    String chatToken = '';
-    try {
-      chatToken = await getChatToken();
-    } catch (e) {
-      print(e);
-      if (e == "AT-C-002") {
-        await AuthService.getAccessToken();
-        chatToken = await getChatToken();
-      }
+  static Future<void> connectWebSocket() async {
+    var url = '$domainUrl/ws';
+
+    var jwtToken = await APIService.storage.read(key: 'token') ?? '';
+
+    stompClient = StompClient(
+      config: StompConfig(
+        url: url,
+        onConnect: onStompConnect,
+        onWebSocketError: (dynamic error) => print('WebSocket Error: $error'),
+        onStompError: (StompFrame frame) => print('STOMP Error: ${frame.body}'),
+        onDisconnect: (frame) => print('Disconnected: ${frame.body}'),
+        onWebSocketDone: () => print('WebSocket Closed'),
+        stompConnectHeaders: {
+          'Authorization': jwtToken,
+        },
+      ),
+    );
+
+    stompClient.activate();
+  }
+
+  static void onStompConnect(StompFrame frame) {
+    print('STOMP 연결 성공');
+    isConnected = true;
+  }
+
+  /*
+    
+    웹소켓 구독 요청
+
+  */
+  static Future<void> subscribeWebSocket(int roomId) async {
+    if (isConnected) {
+      stompClient.subscribe(
+        destination: '/room/$roomId',
+        callback: (StompFrame frame) {
+          if (frame.body != null) {
+            print('Received message from room $roomId: ${frame.body}');
+
+            Map<String, dynamic> messageJson = json.decode(frame.body!);
+            if (messageJson.containsKey('readBy')) {
+              /*
+  
+              구독 후 읽음 처리 메시지 수신
+  
+            */
+              int readBy = messageJson['readBy'];
+              _readReceiptController.add(readBy);
+            } else {
+              /*
+  
+              구독 후 메시지 수신
+  
+            */
+              MessageModel message = MessageModel.fromJson(messageJson);
+              _messageController.add(message);
+            }
+          }
+        },
+      );
     }
-
-    final wsUrl = Uri.parse('ws://3.34.2.246:8081/ws/chat/message/send');
-    final wsReadUrl = Uri.parse('ws://3.34.2.246:8081/ws/chat/message/read');
-    final wsAllReadUrl = Uri.parse('ws://3.34.2.246:8081/ws/chat/room/read');
-    var header = {'Authorization': chatToken};
-    sendChannel = IOWebSocketChannel.connect(wsUrl, headers: header);
-    readChannel = IOWebSocketChannel.connect(wsReadUrl, headers: header);
-    bulkReadChannel = IOWebSocketChannel.connect(wsAllReadUrl, headers: header);
-
-    sendBulkReadRequest(partner);
-
-    sendChannel.stream.listen((message) async {
-      messageSendResponseHandler(
-          message, partner, memberDetails, updateUi, setState);
-    }, onError: (error) {
-      print('Error: $error');
-    }, onDone: () {
-      print('WebSocket connection closed');
-    });
-
-    readChannel.stream.listen((message) async {
-      readResponseHandler(message, setState);
-    }, onError: (error) {
-      print('Error: $error');
-    }, onDone: () {
-      print('WebSocket connection closed');
-    });
-
-    bulkReadChannel.stream.listen((message) async {
-      bulkReadResponseHandler(message, setState);
-    }, onError: (error) {
-      print('Error: $error');
-    }, onDone: () {
-      print('WebSocket connection closed');
-    });
   }
 
   /*
@@ -77,71 +96,47 @@ class ChatService extends APIService {
     메시지 전송 요청
 
   */
-  static void sendMessage(Map<String, dynamic> request) async {
-    sendChannel.sink.add(json.encode(request));
-    requestBuffer.add(request);
-  }
+  static void sendMessage(MessageModel message) {
+    if (isConnected) {
+      Map<String, dynamic> request = {
+        'type': message.type,
+        'content': message.content,
+        'roomId': message.roomId,
+        'senderId': message.senderId,
+        'receiverId': message.receiverId,
+      };
 
-  static void sendVSMessage(Map<String, dynamic> request) async {
-    sendChannel.sink.add(json.encode(request));
-    requestBuffer.add(request);
-  }
-
-  static void sendReadRequest(RemoteMessage message) async {
-    print('단일 읽음처리');
-    Map<String, dynamic> request = {
-      'requestId': DataUtils.makeUUID(),
-      'chatId': message.data['chatId'],
-      'roomId': message.data['roomId'],
-    };
-    readChannel.sink.add(json.encode(request));
-  }
-
-  static void sendBulkReadRequest(Partner partner) async {
-    Map<String, dynamic> request = {
-      'requestId': DataUtils.makeUUID(),
-      'partnerId': partner.memberId,
-      'roomId': partner.roomId,
-    };
-    bulkReadChannel.sink.add(json.encode(request));
-  }
-
-  static void messageSendResponseHandler(message, Partner partner,
-      MemberDetails memberDetails, Function updateUi, Function setState) async {
-    print('웹소켓 전송 Received response: $message');
-    if (json.decode(message)['status'] == 'success') {
-      var requestId = json.decode(message)['requestId'];
-      var request = requestBuffer
-          .firstWhere((element) => element['requestId'] == requestId);
-
-      print(json.decode(message)['chatId']);
-      var chat = MessageModel(
-          id: json.decode(message)['id'],
-          type: request['type'],
-          content: request['content'],
-          roomId: request['roomId'],
-          senderId: request['senderId'],
-          receiverId: request['receiverId'],
-          sendTime: request['sendTime'],
-          isRead: true);
-      await SqlMessageRepository.create(chat);
-      setState(() {});
-
-      requestBuffer.remove(request);
+      stompClient.send(
+        destination: '/chat/send',
+        body: json.encode(request),
+        headers: {'content-type': 'application/json'},
+      );
+      print('Message sent: $request');
+    } else {
+      print('WebSocket is not connected.');
     }
   }
 
-  static void readResponseHandler(message, Function setState) {
-    print('Received response: $message');
-    if (json.decode(message)['status'] == 'success') {
-      setState(() {});
-    }
-  }
+  /*
+   
+    읽음 처리 요청
 
-  static void bulkReadResponseHandler(message, Function setState) async {
-    print('bulk read channel Received response: $message');
-    if (json.decode(message)['status'] == 'success') {
-      setState(() {});
+  */
+  static void sendReadRequest(int roomId, int memberId) async {
+    if (isConnected) {
+      Map<String, dynamic> readRequest = {
+        'roomId': roomId,
+        'memberId': memberId,
+      };
+
+      stompClient.send(
+        destination: '/chat/read',
+        body: json.encode(readRequest),
+        headers: {'content-type': 'application/json'},
+      );
+      print('Read receipt sent: $readRequest');
+    } else {
+      print('WebSocket is not connected.');
     }
   }
 
@@ -169,46 +164,6 @@ class ChatService extends APIService {
       return body.map((dynamic item) => MessageModel.fromJson(item)).toList();
       //fail
     } else {
-      throw Exception('요청 오류');
-    }
-  }
-
-  /*
-
-    채팅 토큰 받아오기
-
-   */
-  static Future<String> getChatToken() async {
-    var url = 'http://3.34.2.246:8080/api/v1/chat/token';
-    //토큰 읽어오기
-    var jwtToken = await APIService.storage.read(key: 'token');
-
-    //accessToken만 보내기
-    jwtToken = json.decode(jwtToken!)['data']['accessToken'];
-
-    var response = await http.get(
-      Uri.parse(url),
-      headers: {
-        'Authorization': 'Bearer $jwtToken',
-        'Content-Type': 'application/json'
-      },
-    );
-
-    //success
-    if (response.statusCode == 200) {
-      return json.decode(utf8.decode(response.bodyBytes))['data'];
-
-      //fail
-    } else {
-      if (json.decode(utf8.decode(response.bodyBytes))['code'] == 'AT-C-002') {
-        // 액세스 토큰 만료
-        throw 'AT-C-002';
-      } else if (json.decode(utf8.decode(response.bodyBytes))['code'] ==
-          'AT-C-007') {
-        // 로그아웃된 토큰
-        throw 'AT-C-007';
-      } else {}
-
       throw Exception('요청 오류');
     }
   }
